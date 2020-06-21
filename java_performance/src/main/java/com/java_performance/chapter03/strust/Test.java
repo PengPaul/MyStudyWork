cd0507bf81678cdab672951f422c6b4f4abfea34
Peng <pengzhao_1990@163.com>
2020-06-21 21:36:31 +0800

    [feature] String 注意点 log

---
+++ java_performance/src/main/java/com/java_performance/chapter03/strust/Test.java
@@ -0,0 +1,12 @@
+package com.java_performance.chapter03.strust;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Test {
+    ArrayList list;
+    Vector vector;
+    LinkedList mLinkedList;
+
+}
