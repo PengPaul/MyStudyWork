61d78d4f50f4062ce75b25b0157bc645e253cbfd
Peng <pengzhao_1990@163.com>
2020-06-21 17:02:04 +0800

    [feature] 单链表 log

--- algorithm/src/main/java/peng/algorithm/chapter02/linkedlist/LinkedListTest.java
+++ algorithm/src/main/java/peng/algorithm/chapter02/linkedlist/LinkedListTest.java
@@ -2,5 +2,28 @@ package peng.algorithm.chapter02.linkedlist;
 
 public class LinkedListTest {
     public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addNode(0,1);
+        myLinkedList.outprintln();
+        myLinkedList.addNode(2);
+        myLinkedList.outprintln();
+        myLinkedList.addNode(3);
+        myLinkedList.outprintln();
+        myLinkedList.addNode(1,4);
+        myLinkedList.outprintln();
+        myLinkedList.addNode(1,5);
+        myLinkedList.outprintln();
+        myLinkedList.addNode(1,6);
+        myLinkedList.outprintln();
+        myLinkedList.addNode(0,7);
+        myLinkedList.outprintln();
+        myLinkedList.addNode(7,8);
+        myLinkedList.outprintln();
+        myLinkedList.deleteNode(2);
+        myLinkedList.outprintln();
+        myLinkedList.deleteNode(0);
+        myLinkedList.outprintln();
+        myLinkedList.deleteNode(5);
+        myLinkedList.outprintln();
     }
 }
