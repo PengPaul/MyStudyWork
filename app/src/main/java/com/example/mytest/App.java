25655c9b4d2bb81a6f9fd41d38310b32a87399a0
Peng <pengzhao_1990@163.com>
2020-06-22 22:53:55 +0800

    [feature] app 启动优化

--- app/src/main/java/com/example/mytest/App.java
+++ app/src/main/java/com/example/mytest/App.java
@@ -7,9 +7,13 @@ import android.util.Log;
 
 import java.io.File;
 
+/**
+ *  优化方案：
+ *   1.开线程     没建handler    没操作UI   对异步要求不高
+ *   2.懒加载     用到的时候再初始化，如网络，数据库操作
+ */
 public class App extends Application {
     private static final String TAG = "App";
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,11 +21,16 @@ public class App extends Application {
         Log.i(TAG,"onCreate:" +file.getAbsolutePath());
         //把分析结果存在文件
         Debug.startMethodTracing(file.getAbsolutePath());
+
+        System.out.println("aaa");
+
         try {
+            //比较耗时的可以在子线程去做
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         Debug.stopMethodTracing();
     }
+
 }
